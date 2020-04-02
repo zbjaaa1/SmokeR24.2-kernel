22c388a8b1da577a605ac699d1a6f925088d5aaa
Tommy Wu <wu.tommy@gmail.com>
2020-04-02 16:00:59 +0800

    update wireguard fetch script

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-USER_AGENT="WireGuard-AndroidROMBuild/0.1 ($(uname -a))"
-# https://git.zx2c4.com/wireguard-monolithic-historical/snapshot/wireguard-monolithic-historical-0.0.20191219.tar.xz
+USER_AGENT="WireGuard-AndroidROMBuild/0.3 ($(uname -a))"
+# https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-1.0.20200401.tar.xz
 
 [[ $(( $(date +%s) - $(stat -c %Y "net/wireguard/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
-[[ $(curl -A "$USER_AGENT" -LSs https://git.zx2c4.com/wireguard-monolithic-historical/refs/) =~ snapshot/wireguard-monolithic-historical-([0-9.]+)\.tar\.xz ]]
+[[ $(curl -A "$USER_AGENT" -LSs https://git.zx2c4.com/wireguard-linux-compat/refs/) =~ snapshot/wireguard-linux-compat-([0-9.]+)\.tar\.xz ]]
 
 if [[ -f net/wireguard/version.h && $(< net/wireguard/version.h) == *${BASH_REMATCH[1]}* ]]; then
 	touch net/wireguard/.check
@@ -14,7 +14,7 @@ fi
 
 rm -rf net/wireguard
 mkdir -p net/wireguard
-curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/wireguard-monolithic-historical/snapshot/wireguard-monolithic-historical-${BASH_REMATCH[1]}.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "wireguard-monolithic-historical-${BASH_REMATCH[1]}/src"
+curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-${BASH_REMATCH[1]}.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "wireguard-linux-compat-${BASH_REMATCH[1]}/src"
 #module
 #sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
 touch net/wireguard/.check
